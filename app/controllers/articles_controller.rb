568d12a9b8ef1a8a9d0516f78e58d34988462776
Nikolaos Emmanouilidis <121699424+NikEmman@users.noreply.github.com>
2024-04-10 18:54:53 +0300

    Add update functionality, form partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,6 +20,20 @@ class ArticlesController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
   
   # strong parameters, whitelists form fields to be added to params
   private
